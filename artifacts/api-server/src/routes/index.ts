7d45ebe9ff171eba36de367cb2f600d9412073a2
avenircc120-debug <267755801+avenircc120-debug@users.noreply.github.com>
2026-08-08 10:52:28 +0100

    Add access routes to deployed API

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router, type IRouter } from "express";
+import accessRouter from "./access";
 import healthRouter from "./health";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
+router.use(accessRouter);
 
 export default router;
